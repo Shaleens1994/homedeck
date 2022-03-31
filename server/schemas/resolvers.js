c2a4e598fd0801d281b2665d99faee21ee5739c2
shaleen shaji <shaleens1994@gmail.com>
2022-03-31 02:49:27 -0400

    resolvers and typedef coerrcetion made and  updated

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -37,7 +37,53 @@ const resolvers = {
     }
   },
 
-  
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addOrder: async (parent, args, context) => {
+      console.log(context);
+      if (context.user) {
+        const order = new Order(args);
+
+        await User.findByIdAndUpdate(context.user._id, { $push: { orders: order } });
+
+        return order;
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+    updateUser: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+    updateProduct: async (parent, { _id, volume }) => {
+      const decrement = Math.abs(volume) * -1;
+
+      return await Product.findByIdAndUpdate(_id, { $inc: { volume: decrement } }, { new: true });
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
     }
 
   }
