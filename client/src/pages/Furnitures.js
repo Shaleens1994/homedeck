b2bc40e9940e2b43d5b1a70f66d35e79f11d6961
abi <abilalu1994@gmail.com>
2022-04-02 18:46:59 -0500

    added comments

--- client/src/pages/Furnitures.js
+++ client/src/pages/Furnitures.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import FurnitureList from '../components/FurnitureList';
 
-
+// to show the page with furniture list
 function Furniture(){
-    
+
     return (
         <div className=" my-1 furnitures">
           <h1 className="mx-4 my-4">Furniture</h1>
