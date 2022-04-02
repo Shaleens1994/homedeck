22e9d87f2bc158d361dad294d7c85be97fdcdfe3
abi <abilalu1994@gmail.com>
2022-04-02 18:45:33 -0500

    wrote function for furniturte

--- client/src/pages/Furnitures.js
+++ client/src/pages/Furnitures.js
@@ -3,6 +3,17 @@ import FurnitureList from '../components/FurnitureList';
 
 
 function Furniture(){
+    
+    return (
+        <div className=" my-1 furnitures">
+          <h1 className="mx-4 my-4">Furniture</h1>
+          <div>
+            <div>
+              <FurnitureList />
+            </div>
+          </div>
+        </div>
+      );
 
 
 }
