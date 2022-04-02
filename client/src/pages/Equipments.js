ca9ae66bb59b7ed1fd2fb6ee6033d1e7b6011c57
abi <abilalu1994@gmail.com>
2022-04-02 18:30:41 -0500

    added function

--- client/src/pages/Equipments.js
+++ client/src/pages/Equipments.js
@@ -4,6 +4,16 @@ import Equipmentlist from '../components/Equipmentlist';
 
 function Equipments(){
 
+    return (
+        <div className=" my-3 equipments">
+          <h1 className="mx-4 my-4">Equipments</h1>
+          <div>
+            <div>
+              <Equipmentlist />
+            </div>
+          </div>
+        </div>
+      );
 }
 
 export default Equipments;
