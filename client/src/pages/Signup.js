282440a3b2d6d51e29ebdd5697c150d24bfc859d
abi <abilalu1994@gmail.com>
2022-04-03 01:25:37 -0500

    added Signup page

---
+++ client/src/pages/Signup.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import Auth from '../utils/auth';
+import { ADD_USER } from '../utils/mutations';
+import rsvp from '../assets/fur.png';
+
+function Signup(props) {
+}
+
+export default Signup;
