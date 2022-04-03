74657b8fa4a51bffd092212032bf712203014e21
abi <abilalu1994@gmail.com>
2022-04-03 12:41:42 -0500

    added cart drawer component

---
+++ client/src/components/CartDrawer/index.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import CartItem from '../CartItem';
+import { useStoreContext } from '../../utils/GlobalState';
+import Auth from '../../utils/auth';
+import { BsXLg } from "react-icons/bs";
+import 'react-dates/initialize';
+import { DateRangePicker } from 'react-dates';
+import "react-dates/lib/css/_datepicker.css";
+import { idbPromise } from "../../utils/helpers";
+
+function CartDrawer(props) {
+
+
+}
+
+export default CartDrawer;
