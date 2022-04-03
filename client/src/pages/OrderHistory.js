c43c6041fb69f5702ff62a6a7dbf01aff1693a6b
abi <abilalu1994@gmail.com>
2022-04-02 19:01:33 -0500

    added order history page

---
+++ client/src/pages/OrderHistory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../utils/queries';
+
+function OrderHistory() {
+
+
+       );
+   }
+
+export default OrderHistory;
