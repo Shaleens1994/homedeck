701dd48bfd958977911f69d6858f50c17cf861f8
shaleen shaji <shaleens1994@gmail.com>
2022-04-03 13:38:10 -0400

    date picke r react

---
+++ client/src/components/DatePicker/index.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const RSVPDatePicker = () => {
+
+
+
+
+
+
+};
+
+export default RSVPDatePicker;
