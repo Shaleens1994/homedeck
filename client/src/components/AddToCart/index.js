3e7edff8d1e416a3c27c58bed679a5e3866188c6
abi <abilalu1994@gmail.com>
2022-04-03 12:30:47 -0500

    added add to cart component

---
+++ client/src/components/AddToCart/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useParams } from 'react-router-dom';
+import spinner from '../../assets/spinner.gif';
+import Cart from "../Cart";
+import { useQuery } from '@apollo/client';
+import {QUERY_PRODUCTS} from '../../utils/queries';
+import { useStoreContext } from "../../utils/GlobalState";
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_DAYS,
+  ADD_TO_CART,
+} from "../../utils/actions";
+
+function AddToCart(input) {
+
+
+
+
+    
+}
+
+export default AddToCart;
