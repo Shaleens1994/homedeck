4d01c4e9b06976d7df34f3da60c6d189e8bb76d7
abi <abilalu1994@gmail.com>
2022-04-03 14:06:39 -0500

    added product item

---
+++ client/src/components/ProductItemDetail/index.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_PRODUCTS } from '../../utils/queries';
+import AddToCart from '../AddToCart';
+
+import { useStoreContext } from "../../utils/GlobalState";
+import { UPDATE_PRODUCTS
+} from "../../utils/actions";
+
+function ProductItemDetail(item) {
+    
+
+
+
+
+
+}
+
+export default ProductItemDetail;
