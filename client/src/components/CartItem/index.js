27ebb96129a93ac5d8f318129e7a728bf1c6754f
shaleen shaji <shaleens1994@gmail.com>
2022-04-03 13:32:17 -0400

    cart item react

---
+++ client/src/components/CartItem/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useStoreContext } from '../../utils/GlobalState';
+import { REMOVE_FROM_CART, UPDATE_CART_DAYS } from '../../utils/actions';
+import { idbPromise } from "../../utils/helpers";
+const CartItem = ({ item }) => {
+
+
+
+}
+
+export default CartItem;
