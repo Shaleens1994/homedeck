2bcce1cd457476f82fc3ef3505ce44e5f0f0f87d
shaleen shaji <shaleens1994@gmail.com>
2022-04-03 14:58:17 -0400

    nav bar

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import Auth from "../../utils/auth";
+import { Link, NavLink } from "react-router-dom";
+import { AiOutlineMenu } from "react-icons/ai";
+
+function Nav() {
+
+
+
+
+
+}
+
+export default Nav;
