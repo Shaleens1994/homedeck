cf0eff642011eb5a2929a769df49ddd3a63d3ae5
abi <abilalu1994@gmail.com>
2022-04-03 01:29:52 -0500

    added success page

---
+++ client/src/pages/Success.js
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_ORDER } from '../utils/mutations';
+import { idbPromise } from '../utils/helpers';
+
+function Success() {
+};
+
+export default Success;
