55b014db5946567c4d85c2669d12b8eda34e05f1
abi <abilalu1994@gmail.com>
2022-04-03 12:37:03 -0500

    added cart component

---
+++ client/src/components/Cart/index.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import CartDrawer from '../CartDrawer';
+import CartDrawerBackdrop from '../CartDrawerBackdrop';
+import { useLazyQuery } from '@apollo/client';
+import { useStoreContext } from '../../utils/GlobalState'
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART} from '../../utils/actions';
+import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+const stripePromise = loadStripe('');
+
+const Cart = () => {
+
+};
+
+export default Cart;
