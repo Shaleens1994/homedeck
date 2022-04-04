1683b1c34d1920727f6c98691c81ca42e96e49f2
shaleen shaji <shaleens1994@gmail.com>
2022-04-04 08:47:26 -0400

    cleaning main to merge

--- client/src/components/CartDrawerBackdrop/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function CartDrawerBackdrop(props) {
-  return (
-    <div className="cartbackground" onClick={props.toggleCart} />
-  )
-}
-
-export default CartDrawerBackdrop;
