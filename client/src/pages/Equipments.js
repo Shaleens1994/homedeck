873be7a2e824413e0233590832e700aa328cd974
Nevan <nev.kd2832@gmail.com>
2022-04-04 02:09:49 -0400

    added page requirements

--- client/src/pages/Equipments.js
+++ client/src/pages/Equipments.js
@@ -1,19 +1,3 @@
 import React from 'react';
 import Equipmentlist from '../components/Equipmentlist';
 
-// to display the equipment lists
-function Equipments(){
-
-    return (
-        <div className=" my-3 equipments">
-          <h1 className="mx-4 my-4">Equipments</h1>
-          <div>
-            <div>
-              <Equipmentlist />
-            </div>
-          </div>
-        </div>
-      );
-}
-
-export default Equipments;
